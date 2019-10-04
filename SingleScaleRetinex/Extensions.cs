252f68608c42bc0797a77ba953516445a2fcf82f
Dmitry Vasilevich <wotlk777@gmail.com>
2019-10-05 02:55:29 +0300

    FINALLY FIXED! Now it's working SSR

--- SingleScaleRetinex/Extensions.cs
+++ SingleScaleRetinex/Extensions.cs
@@ -20,129 +20,36 @@ namespace SingleScaleRetinex
         /// </summary>
         public static string ApplySSR(this Image<Rgb, byte> img, int gaussKernelDim, int sigma)
         {
+            // to work only with luminance channel
+            var ycc = img.Convert<Ycc, byte>();
+
             var scales = new[] { 10, 50, 80 };
-            var channels = img.Split();
-            var data = img.Data;
-            var dataCopy = (byte[,,])data.Clone();
+            //var channels = ycc.Split();
+            var data = ycc.Data;
 
-            for (int channel = 0; channel < channels.Length; channel++)
+            for (int channel = 0; channel < 1; channel++)
             {
-                //for (int sc = 0; sc < scales.Length; sc++)
-                //{
-                    var kernel = CreateGaussKernel(sigma, gaussKernelDim);
-                    FlipMatrixByDimensions(kernel, 0, 1);
+                var kernel = CreateGaussKernel(sigma, gaussKernelDim);
+                FlipMatrixByDimensions(kernel, 0, 1);
 
-                    var convolved = ConvolveValues(dataCopy, kernel, channel);
+                var convolved = ConvolveValues(data, kernel, channel);
 
-                    for (int i = 0; i < img.Rows; i++)
+                for (int i = 0; i < img.Rows; i++)
+                {
+                    for (int j = 0; j < img.Cols; j++)
                     {
-                        for (int j = 0; j < img.Cols; j++)
-                        {
-                            var value = Math.Log(data[i, j, channel]) - Math.Log(convolved[i, j, channel]);
-                            //var value = Math.Log(data[i, j, channel] / convolved[i, j, channel]);
-                            value = value * 255 + 127.5;
+                        var value = Math.Log(data[i, j, channel]) - Math.Log(convolved[i, j, channel]);
+                        value = value * 255 + 127.5;
 
-                            data[i, j, channel] = (byte)value;
-                        }
+                        data[i, j, channel] = (byte)value;
                     }
-                //}
+                }
             }
 
-            //var c = ConvolveValues(dataCopy, kernel, 0);
-            //c = ConvolveValues(c, kernel, 1);
-            //c = ConvolveValues(c, kernel, 2);
-
-            //var image = new Image<Rgb, byte>(c.GetUpperBound(0) + 1, c.GetUpperBound(1) + 1)
-            //{
-            //    Data = c
-            //};
-            //image.Save($"aaa.jpg");
+            img = ycc.Convert<Rgb, byte>();
 
             var savePath = $"{DateTime.Now.ToString("HH_mm_")}123.jpg";
             img.Save(savePath);
-
-            return savePath;
-        }
-
-        public static string SingleScaleRetinex(this Image<Rgb, byte> img, int gaussianKernelSize, double sigma)
-        {
-            var radius = gaussianKernelSize / 2;
-            var kernelSize = 2 * radius + 1;
-
-            var ycc = img.Convert<Ycc, byte>();
-
-            // Формируем ядро Гауссиана
-            var sum = 0f;
-            var gaussKernel = new float[kernelSize * kernelSize];
-            for (int i = -radius, k = 0; i <= radius; i++, k++)
-            {
-                for (int j = -radius; j <= radius; j++)
-                {
-                    var val = (float)Math.Exp(-(i * i + j * j) / (sigma * sigma));
-                    gaussKernel[k] = val;
-                    sum += val;
-                }
-            }
-            for (int i = 0; i < gaussKernel.Length; i++)
-                gaussKernel[i] /= sum;
-
-            // Работаем только с яркостным каналом
-            var gray = new Image<Gray, byte>(ycc.Size);
-            CvInvoke.cvSetImageCOI(ycc, 1);
-            CvInvoke.cvCopy(ycc, gray, IntPtr.Zero);
-
-            // Размеры изображения
-            var width = img.Width;
-            var height = img.Height;
-
-            var bmp = gray.Bitmap;
-            var bitmapData = bmp.LockBits(new Rectangle(Point.Empty, gray.Size), ImageLockMode.ReadWrite, PixelFormat.Format8bppIndexed);
-
-            unsafe
-            {
-                for (var y = 0; y < height; y++)
-                {
-                    var row = (byte*)bitmapData.Scan0 + y * bitmapData.Stride;
-                    for (var x = 0; x < width; x++)
-                    {
-                        var color = row + x;
-
-                        float val = 0;
-
-                        for (int i = -radius, k = 0; i <= radius; i++, k++)
-                        {
-                            var ii = y + i;
-                            if (ii < 0) ii = 0; if (ii >= height) ii = height - 1;
-
-                            var row2 = (byte*)bitmapData.Scan0 + ii * bitmapData.Stride;
-                            for (int j = -radius; j <= radius; j++)
-                            {
-                                var jj = x + j;
-                                if (jj < 0) jj = 0; if (jj >= width) jj = width - 1;
-
-                                val += *(row2 + jj) * gaussKernel[k];
-
-                            }
-                        }
-
-                        var newColor = 127.5 + 255 * Math.Log(*color / val);
-                        if (newColor > 255)
-                            newColor = 255;
-                        else if (newColor < 0)
-                            newColor = 0;
-                        *color = (byte)newColor;
-                    }
-                }
-            }
-            bmp.UnlockBits(bitmapData);
-
-            CvInvoke.cvCopy(gray, ycc, IntPtr.Zero);
-            CvInvoke.cvSetImageCOI(ycc, 0);
-
-            var image = ycc.Convert<Bgr, byte>();
-
-            var savePath = $"{DateTime.Now.ToString("HH_mm_")}1234.jpg";
-            image.Save(savePath);
 
             return savePath;
         }
@@ -235,7 +142,7 @@ namespace SingleScaleRetinex
                 {
                     var sum = 0.0;
 
-                    // [kernel] values -- coords in matrix according to kernel
+                    // [kernel] values -- coords in matrix assuming [0,0] is kernel center
                     // [i, j] -- kernel 0-based coords
                     for (int kernelY = -offsetY, j = 0; kernelY <= offsetY; kernelY++, j++)
                     {
@@ -303,24 +210,6 @@ namespace SingleScaleRetinex
             }
 
             return convolved;
-        }
-
-        private static T[][] ToJaggedArrayRows<T>(this T[,] arr)
-        {
-            return Enumerable.Range(0, arr.GetUpperBound(0) + 1)
-                             .Select(i => Enumerable.Range(0, arr.GetUpperBound(1) + 1)
-                                                    .Select(j => arr[i, j])
-                                                    .ToArray())
-                             .ToArray();
-        }
-
-        private static T[][] ToJaggedArrayCols<T>(this T[,] arr)
-        {
-            return Enumerable.Range(0, arr.GetUpperBound(1) + 1)
-                             .Select(j => Enumerable.Range(0, arr.GetUpperBound(0) + 1)
-                                                    .Select(i => arr[j, i])
-                                                    .ToArray())
-                             .ToArray();
         }
     }
 }
--- SingleScaleRetinex/Program.cs
+++ SingleScaleRetinex/Program.cs
@@ -13,20 +13,10 @@ namespace SingleScaleRetinex
         static void Main(string[] args)
         {
             Console.Write("Path: ");
-
-            //var path = Console.ReadLine();
-            //var image = new Image<Rgb, byte>(path.Replace("\"", ""));
-
             var path = Console.ReadLine();
-            if (path == "1")
-                path = @"C:\Users\Dmitry Vasilevich\Desktop\24.jpg";
 
             var image = new Image<Rgb, byte>(path.Replace("\"", ""));
-
-            //image.ApplySSR(7, 80);
-
-            var proc = System.Diagnostics.Process.Start(image.ApplySSR(5, 80));
-            var proc1 = System.Diagnostics.Process.Start(Extensions.SingleScaleRetinex(image, 5, 80));
+            System.Diagnostics.Process.Start(image.ApplySSR(7, 80));
         }
     }
 }
