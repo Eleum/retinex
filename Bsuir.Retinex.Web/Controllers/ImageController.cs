6df41d56bc92fe83277b1a8662c3417456659532
Dmitry Vasilevich <wotlk777@gmail.com>
2019-10-06 01:49:01 +0300

    add sample Web API proj

---
+++ Bsuir.Retinex.Web/Controllers/ImageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bsuir.Retinex.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Content("Working...");
+        }
+    }
+}
