40d31b4cafb90e324ceb18749c63a14a1b3231b5
Dmitry Vasilevich <wotlk777@gmail.com>
2019-10-16 01:56:54 +0300

    working app with open cv methods

--- SingleScaleRetinex/Extensions.cs
+++ SingleScaleRetinex/Extensions.cs
@@ -12,43 +12,364 @@ namespace SingleScaleRetinex
 {
     public static class Extensions
     {
-        /// <summary>
-        /// Applies Single Scale Retinex to the image
-        /// <paramref name="img">Image to process</paramref>
-        /// <paramref name="gaussKernelDim">Gaussian kernel dimension</paramref>
-        /// <paramref name="sigma">Width of the Gaussian kerner</paramref>
-        /// </summary>
-        public static string ApplySSR(this Image<Rgb, byte> img, int gaussKernelDim, int sigma)
+
+    /// <summary>
+    /// Applies Single Scale Retinex to the image
+    /// <paramref name="image">Image to process</paramref>
+    /// <paramref name="gaussKernelDim">Gaussian kernel dimension</paramref>
+    /// <paramref name="sigma">Width of the Gaussian kerner</paramref>
+    /// </summary>
+    public static string ApplySSR(this Image<Bgr, byte> img, int gaussKernelDim, int sigma)
         {
             // to work only with luminance channel
-            var ycc = img.Convert<Ycc, byte>();
+            var image = img.Convert<Bgr, double>();
 
-            var scales = new[] { 10, 50, 80 };
-            //var channels = ycc.Split();
+            //MyFilter(ref image, sigma);
+
+            //Environment.Exit(0);
+
+            //var gray = new Image<Gray, byte>(ycc.Size);
+            //CvInvoke.cvSetImageCOI(ycc, 1);
+            //CvInvoke.cvCopy(ycc, gray, IntPtr.Zero);
+
+            //var data = image.Data;
+
+            var A = new Image<Bgr, double>(image.Size);
+            var AA = new Image<Bgr, double>(image.Size);
+            var imgA = new Image<Bgr, double>(image.Size);
+            var imgB = new Image<Bgr, double>(image.Size);
+            var imgC = new Image<Bgr, double>(image.Size);
+
+            //CvInvoke.cvCopy(image, imgLog, IntPtr.Zero);
+            //CvInvoke.cvCopy(image, imgConvLog, IntPtr.Zero);
+            //CvInvoke.cvCopy(image, imgSub, IntPtr.Zero);
+
+            CvInvoke.cvConvertScale(image, imgA, 1, 0);
+            CvInvoke.Log(imgA, imgB);
+
+            A = image.Clone();
+            AA = image.Clone();
+
+            //Filter(ref A, sigma);
+            MyFilter(ref A, sigma);
+
+            //var kernel = CreateGaussKernel(sigma, gaussKernelDim);
+            //FlipMatrixByDimensions(kernel, 0, 1);
+            //var convolved = new Image<Bgr, double>(ConvolveValues(AA.Data, kernel));
+            //CvInvoke.cvConvertScale(convolved, imgA, 1, 0);
+
+            CvInvoke.cvConvertScale(A, imgA, 1, 0);
+            CvInvoke.Log(imgA, imgC);
+
+            CvInvoke.Subtract(imgB, imgC, imgA);
+
+            CvInvoke.cvConvertScale(imgA, image, 128, 128);
+
+            var ptrImg = img.Ptr;
+            var Aptr = A.Ptr;
+            var ptrA = imgA.Ptr;
+            var ptrB = imgB.Ptr;
+            var ptrC = imgC.Ptr;
+
+            CvInvoke.cvReleaseImage(ref ptrImg);
+            CvInvoke.cvReleaseImage(ref Aptr);
+            CvInvoke.cvReleaseImage(ref ptrA);
+            CvInvoke.cvReleaseImage(ref ptrB);
+            CvInvoke.cvReleaseImage(ref ptrC);
+
+            var savePath = $"{DateTime.Now.ToString("HH_mm_")}123.jpg";
+            image.Save(savePath);
+
+            return savePath;
+
+
+            //CvInvoke.Log(image, imgConvLog);
+
+            //var imageConvolved = new Image<Bgr, double>(convolved);
+
+            //CvInvoke.Subtract(imgLog, imgConvLog, imgSub);
+
+            //for (int channel = 0; channel < 3; channel++)
+            //{
+            //    var convolved = ConvolveValues(data, kernel, channel);
+
+            //    for (int i = 0; i < img.Rows; i++)
+            //    {
+            //        for (int j = 0; j < img.Cols; j++)
+            //        {
+            //            var value = Math.Log(data[i, j, channel]) - Math.Log(convolved[i, j, channel]);
+            //            //value = value * 128 + 128;
+
+            //            //if (value > 255)
+            //            //    value = 255;
+            //            //if (value < 0)
+            //            //    value = 0;
+
+            //            data[i, j, channel] = (byte)value;
+            //        }
+            //    }
+            //}
+
+            //CvInvoke.cvCopy(gray, ycc, IntPtr.Zero);
+            //CvInvoke.cvSetImageCOI(ycc, 0);
+
+            //img = ycc.Convert<Bgr, byte>();
+
+            //CvInvoke.cvConvertScale(imgSub, image, 128, 128);
+
+            //var savePath = $"{DateTime.Now.ToString("HH_mm_")}123.jpg";
+            //image.Save(savePath);
+
+            //return savePath;
+        }
+
+        public static void MyFilter(ref Image<Bgr, double> img, int sigma)
+        {
+            if (sigma > 300) 
+                sigma = 300;
+
+            var filterSize = (sigma * 6) + 1;
+
+            if (filterSize < 3)
+                return;
+
+            if (filterSize < 10)
+            {
+                CvInvoke.GaussianBlur(img, img, new Size(), filterSize, filterSize);
+            }
+            else
+            {
+                if (img.Height < 2 || img.Width < 2)
+                    return;
+
+                var sub = new Image<Bgr, double>(img.Width / 2, img.Height / 2);
+                var subPtr = sub.Ptr;
+                CvInvoke.PyrDown(img, sub);
+
+                //var sub1 = new Image<Bgr, double>(img1.Width / 2, img1.Height / 2);
+                //Downsample(img1, ref sub1);
+
+                MyFilter(ref sub, sigma/2);
+
+                CvInvoke.Resize(sub, img, img.Size);
+                CvInvoke.cvReleaseImage(ref subPtr);
+            }
+        }
+
+        private static void Downsample(Image<Bgr, double> src, ref Image<Bgr, double> dest)
+        {
+            var arr = new double[dest.Data.GetLength(0), dest.Data.GetLength(1), 3];
+
+            var kernel = GetGaussPyramidKernel();
+            var data = ConvolveValues(src.Data, kernel, true);
+
+            for (int channel = 0; channel < dest.NumberOfChannels; channel++)
+            {
+                for (int i = 0, row = 0; i < dest.Data.GetLength(1); i++)
+                {
+                    if ((i + 1) % 2 == 0)
+                        continue;
+
+                    for (int j = 0, col = 0; j < dest.Data.GetLength(0); j++)
+                    {
+                        if ((j + 1) % 2 == 0)
+                            continue;
+
+                        arr[col++, row, channel] = data[j, i, channel];
+                    }
+
+                    row++;
+                }
+            }
+
+            dest.Data = arr;
+        }
+
+        private static void Filter(ref Image<Bgr, double> img, int sigma)
+        {
+            if (sigma > 300) sigma = 300;
+
+            var filterSize = (int)Math.Floor(sigma * 6 * 0.5);
+            filterSize = filterSize * 2 + 1;
+
+            if (filterSize < 3)
+                return;
+
+            if (filterSize < 10)
+            {
+                CvInvoke.GaussianBlur(img, img, new Size(), filterSize, filterSize);
+            }
+            else
+            {
+                if (img.Height < 2 || img.Width < 2)
+                    return;
+
+                var sub = new Image<Bgr, double>(img.Width / 2, img.Height / 2);
+                var subPtr = sub.Ptr;
+
+                CvInvoke.PyrDown(img, sub);
+                Filter(ref sub, sigma/2);
+                CvInvoke.Resize(sub, img, img.Size);
+                CvInvoke.cvReleaseImage(ref subPtr);
+            }
+        }
+
+        public static Image<Bgr, double> FilterGaussian(Image<Bgr, double> img, int sigma)
+        {
+            var kernel = CreateGaussKernel(sigma);
+            FlipMatrixByDimensions(kernel, 0, 1);
+            return new Image<Bgr, double>(ConvolveValues(img.Data, kernel));
+        }
+
+        public static string ApplyMSR(this Image<Bgr, byte> img, int gaussKernelDim)
+        {
+            var scales = new[] { 15, 80, 250 };
+            var channels = img.Split();
+            var data1 = img.Data;
+
+            var ycc = img.Convert<Ycc, byte>();
             var data = ycc.Data;
+
+            var diffs = new double[3];
 
             for (int channel = 0; channel < 1; channel++)
             {
-                var kernel = CreateGaussKernel(sigma, gaussKernelDim);
-                FlipMatrixByDimensions(kernel, 0, 1);
-
-                var convolved = ConvolveValues(data, kernel, channel);
+                var convolutions = CreateConvolutions(data, CreateKernels(scales, gaussKernelDim), channel);
 
                 for (int i = 0; i < img.Rows; i++)
                 {
                     for (int j = 0; j < img.Cols; j++)
                     {
-                        var value = Math.Log(data[i, j, channel]) - Math.Log(convolved[i, j, channel]);
-                        value = value * 255 + 127.5;
+                        for (var scale = 0; scale < scales.Length; scale++)
+                        {
+                            var value = Math.Log(data[i, j, channel]) - Math.Log(convolutions[scale][i, j, channel]);
+                            diffs[scale] = value;
+                        }
 
-                        data[i, j, channel] = (byte)value;
+                        var val = diffs.Sum(x => x / 3) * 255 + 127.5;
+
+                        //if (val > 255)
+                        //    val = 255;
+                        if (val < 0)
+                            val = Math.Log(0.01);
+
+                        data[i, j, channel] = (byte)val;
                     }
                 }
             }
 
-            img = ycc.Convert<Rgb, byte>();
+            img = ycc.Convert<Bgr, byte>();
 
             var savePath = $"{DateTime.Now.ToString("HH_mm_")}123.jpg";
+            img.Save(savePath);
+
+            return savePath;
+        }
+
+        private static double[][,] CreateKernels(int[] scales, int gaussKernelDim)
+        {
+            var kernels = new List<double[,]>();
+
+            for (int i = 0; i < scales.Length; i++)
+            {
+                var kernel = CreateGaussKernel(scales[i], gaussKernelDim);
+                FlipMatrixByDimensions(kernel, 0, 1);
+                kernels.Add(kernel);
+            }
+
+            return kernels.ToArray();
+        }
+
+        private static double[][,,] CreateConvolutions(byte[,,] data, double[][,] kernels, int channel)
+        {
+            //var convolutions = new List<double[,,]>();
+
+            //foreach (var kernel in kernels)
+            //{
+            //    var convolved = ConvolveValues(data, kernel, channel);
+            //    convolutions.Add(convolved);
+            //}
+
+            //return convolutions.ToArray();
+            return null;
+        }
+
+        public static string SingleScaleRetinex(this Image<Bgr, byte> img, int gaussianKernelSize, double sigma)
+        {
+            var radius = gaussianKernelSize / 2;
+            var kernelSize = 2 * radius + 1;
+
+            var ycc = img.Convert<Ycc, byte>();
+
+            var sum = 0f;
+            var gaussKernel = new float[kernelSize * kernelSize];
+            for (int i = -radius, k = 0; i <= radius; i++, k++)
+            {
+                for (int j = -radius; j <= radius; j++)
+                {
+                    var val = (float)Math.Exp(-(i * i + j * j) / (sigma * sigma));
+                    gaussKernel[k] = val;
+                    sum += val;
+                }
+            }
+            for (int i = 0; i < gaussKernel.Length; i++)
+                gaussKernel[i] /= sum;
+
+            var gray = new Image<Gray, byte>(ycc.Size);
+            CvInvoke.cvSetImageCOI(ycc, 1);
+            CvInvoke.cvCopy(ycc, gray, IntPtr.Zero);
+
+            // Размеры изображения
+            var width = img.Width;
+            var height = img.Height;
+
+            var bmp = gray.Bitmap;
+            var bitmapData = bmp.LockBits(new Rectangle(Point.Empty, gray.Size), ImageLockMode.ReadWrite, PixelFormat.Format8bppIndexed);
+
+            unsafe
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    var row = (byte*)bitmapData.Scan0 + y * bitmapData.Stride;
+                    for (var x = 0; x < width; x++)
+                    {
+                        var color = row + x;
+
+                        float val = 0;
+
+                        for (int i = -radius, k = 0; i <= radius; i++, k++)
+                        {
+                            var ii = y + i;
+                            if (ii < 0) ii = 0; if (ii >= height) ii = height - 1;
+
+                            var row2 = (byte*)bitmapData.Scan0 + ii * bitmapData.Stride;
+                            for (int j = -radius; j <= radius; j++)
+                            {
+                                var jj = x + j;
+                                if (jj < 0) jj = 0; if (jj >= width) jj = width - 1;
+
+                                val += *(row2 + jj) * gaussKernel[k];
+
+                            }
+                        }
+
+                        var newColor = 127.5 + 255 * Math.Log(*color / val);
+                        if (newColor > 255)
+                            newColor = 255;
+                        else if (newColor < 0)
+                            newColor = 0;
+                        *color = (byte)newColor;
+                    }
+                }
+            }
+            bmp.UnlockBits(bitmapData);
+
+            CvInvoke.cvCopy(gray, ycc, IntPtr.Zero);
+            CvInvoke.cvSetImageCOI(ycc, 0);
+
+            img = ycc.Convert<Bgr, byte>();
+
+            var savePath = $"{DateTime.Now.ToString("HH_mm_")}SSR.jpg";
             img.Save(savePath);
 
             return savePath;
@@ -124,7 +445,19 @@ namespace SingleScaleRetinex
             return gaussKernel;
         }
 
-        private static byte[,,] ConvolveValues(byte[,,] input, double[,] kernel, int channel)
+        private static double[,] GetGaussPyramidKernel()
+        {
+            return new double[,]
+            {
+                { 1, 4, 6, 4, 1},
+                { 4, 16, 24, 16, 4},
+                { 6, 24, 36, 24, 6},
+                { 4, 16, 24, 16, 4},
+                { 1, 4, 6, 4, 1},
+            };
+        }
+
+        private static double[,,] ConvolveValues(double[,,] input, double[,] kernel, bool useGaussPyramid = false, int channel = -1)
         {
             var height = input.GetLength(0);
             var width = input.GetLength(1);
@@ -134,34 +467,40 @@ namespace SingleScaleRetinex
             var offsetY = sizeY / 2;
             var offsetX = sizeX / 2;
 
-            var convolved = (byte[,,])input.Clone();
+            var convolved = new double[input.GetLength(0), input.GetLength(1), 3];
 
-            for (int y = 0; y < height; y++)
+            // to keep data of channels which are not processed
+            Array.Copy(input, convolved, input.GetLength(0) * input.GetLength(1));
+
+            for (int ch = (channel == -1 ? 0 : channel); ch < (channel == -1 ? 3 : channel + 1); ch++)
             {
-                for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
                 {
-                    var sum = 0.0;
-
-                    // [kernel] values -- coords in matrix assuming [0,0] is kernel center
-                    // [i, j] -- kernel 0-based coords
-                    for (int kernelY = -offsetY, j = 0; kernelY <= offsetY; kernelY++, j++)
+                    for (int x = 0; x < width; x++)
                     {
-                        for (int kernelX = -offsetX, i = 0; kernelX <= offsetX; kernelX++, i++)
+                        var sum = 0.0;
+
+                        // [kernel] values -- coords in matrix assuming [0,0] is kernel center
+                        // [i, j] -- kernel 0-based coords
+                        for (int kernelY = -offsetY, j = 0; kernelY <= offsetY; kernelY++, j++)
                         {
-                            var inputY = y + kernelY;
-                            var inputX = x + kernelX;
+                            for (int kernelX = -offsetX, i = 0; kernelX <= offsetX; kernelX++, i++)
+                            {
+                                var inputY = y + kernelY;
+                                var inputX = x + kernelX;
 
-                            if (inputX < 0 || inputY < 0)
-                                continue;
+                                if (inputX < 0 || inputY < 0)
+                                    continue;
 
-                            if (inputX >= width || inputY >= height)
-                                continue;
+                                if (inputX >= width || inputY >= height)
+                                    continue;
 
-                            sum += input[inputY, inputX, channel] * kernel[j, i];
+                                sum += input[inputY, inputX, ch] * kernel[j, i] * (useGaussPyramid ? 1.0/256 : 1);
+                            }
                         }
-                    }
 
-                    convolved[y, x, channel] = (byte)sum;
+                        convolved[y, x, ch] = sum;
+                    }
                 }
             }
 
--- SingleScaleRetinex/Program.cs
+++ SingleScaleRetinex/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Emgu.CV;
+using Emgu.CV.Bioinspired;
 using Emgu.CV.Structure;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,8 +17,11 @@ namespace SingleScaleRetinex
             Console.Write("Path: ");
             var path = Console.ReadLine();
 
-            var image = new Image<Rgb, byte>(path.Replace("\"", ""));
+            var image = new Image<Bgr, byte>(path.Replace("\"", ""));
+
             System.Diagnostics.Process.Start(image.ApplySSR(7, 80));
+            //System.Diagnostics.Process.Start(image.ApplyMSR(7));
+            //System.Diagnostics.Process.Start(Extensions.SingleScaleRetinex(image, 7, 80));
         }
     }
 }
